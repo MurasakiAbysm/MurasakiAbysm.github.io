6d1bed2b8131687ec95e7686a2c852ad998f6b80
MurasakiAbysm <GitHub 2428335168@qq.com>
2024-07-27 15:58:08 +0800

    Site updated: 2024-07-27 15:58:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/26/我的博客会越来越好的/","2024/07/24/这是我的第一篇博客文章/","2024/07/24/论Python/"];function toRandomPost(){
+var posts=["2024/07/26/我的博客会越来越好的/","2024/07/27/Python基础学习/","2024/07/24/这是我的第一篇博客文章/","2024/07/24/论Python/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
